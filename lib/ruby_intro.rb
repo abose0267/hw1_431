55a57354f6ad7f71b285c345e0abf41356908e1f
Arka Bose <66583867+abose0267@users.noreply.github.com>
2022-12-21 01:54:38 -0600

    push

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,85 @@
 
 def sum(arr)
   # YOUR CODE HERE
+  value = 0
+  arr.each { |i|
+     value += i
+  }
+  # puts value
+  return value
+
 end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
+
+temp = arr.max(2)
+val  = 0
+temp.each { |i|
+ val +=i
+}
+return val
 end
 
 def sum_to_n?(arr, number)
   # YOUR CODE HERE
+
+  arr.each { |i|
+  val = number-i
+  count = 0
+  puts
+  puts "#{number} - #{i} = #{val}"
+  puts arr.include? val
+  puts 
+  if arr.include? val
+    if val != i
+      return true
+    end
+    return false
+  end
+}
+return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  # puts "Hello " + name
+  return "Hello, " + name
 end
 
 def starts_with_consonant?(string)
   # YOUR CODE HERE
+  sadVals = "AaEeIiOoUuYy!@#$%^&*()_-+="
+  # puts 
+  # puts string[0]
+  # puts
+
+  if string[0] != nil
+    # puts "NOt nil"
+    if sadVals.include?string[0] 
+      return false
+    end
+    return true
+  end
+
+  return false
+
 end
 
 def binary_multiple_of_4?(string)
   # YOUR CODE HERE
+  # puts string
+  # puts string.to_i(2)
+  # puts string.to_i(2) % 4
+  # puts
+  if string.to_i(2) % 4 == 0
+    return true
+  end
+  return false
+
+
 end
 
 # Part 3
@@ -33,4 +90,32 @@ end
 # Object representing a book
 class BookInStock
   # YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn == "" || price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+
+##Getter and Setter methods 
+  def isbn()
+    return @isbn
+  end
+
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+
+  def price()
+    return @price
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price_as_string()
+    return "$" + sprintf("%.2f", @price)
+  end
 end
